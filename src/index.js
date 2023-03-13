178de80596a5020f45c97971563797b28327f1f1
seyha30 <sinseyha1x@gmail.com>
2023-03-13 22:36:24 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+
+const button = <button onClick="activateLasers">Activate Lasers</button>;
+function activateLasers() {
+  console.log(111);
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggle: true };
+    this.handleClcik = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggle,
+    }));
+  }
+  render() {
+    return (
+      <div>
+        return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? "ON" : "OFF"}
+        </button>
+        );
+      </div>
+    );
+  }
+}
+
+root.render(button);
